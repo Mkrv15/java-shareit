50de1b0004d841a7f55549ca797ac5b4aa18c913
Mkrv15 <ilya15040@yandex.ru>
2026-06-08 23:51:41 +0400

    fix tests db

--- src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.request.controller;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping(path = "/requests")
-public class ItemRequestController {
-}
